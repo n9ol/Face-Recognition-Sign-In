b6ccd2ce0e1c4de4bb4ae3046a0c20fbc7da23eb
wsc <jieniself@hotmail.com>
2019-05-24 20:03:54 +0800

    test cloud function finished

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,6 +2,17 @@ Page({
   onTapJumpStu: function(event) {
     wx.navigateTo({
       url: '../stuLog/stuLog',
+    });
+    wx.cloud.callFunction({
+      name: 'add',
+      data: {
+        a: 1,
+        b: 2,
+      },
+      success(res) {
+        console.log(res.result.sum);
+      },
+      fail: console.error
     })
   },
 
